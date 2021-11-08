1e70b1db5f50362cd60b238d7e6186612010fa2e
vagrant <vagrant@vagrant.vm>
2021-11-08 09:45:39 +0000

    Editing modifiers Game.h

--- classes/Game.h
+++ classes/Game.h
@@ -9,6 +9,11 @@ public:
 	Game();
 	void solve(unsigned solution);
 
+	friend std::ostream &operator<<(std::ostream &os, const Game &game);
+
+private:
+	std::vector<Stack> _stacks;
+
 	void solve_first();
 	void _legalMove(unsigned position_a, unsigned position_b);
 	void _move(unsigned position_a, unsigned postition_b);
@@ -16,9 +21,4 @@ public:
 	void solve_second();
 	void _move(unsigned start, unsigned end, Stack &source, Stack &target, Stack &spare);
 	void _moveCube(Stack &stack_one, Stack &stack_two);
-
-	friend std::ostream &operator<<(std::ostream &os, const Game &game);
-
-private:
-	std::vector<Stack> _stacks;
 };
